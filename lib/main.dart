3d418cc21592e5bd5a3ba2f98ae5b2e667f1a4c9
jaehee21232 <a62533196@gmail.com>
2023-04-25 00:25:20 +0900

    feat: alarm_manager add

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,30 @@
+import 'dart:isolate';
+
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:app_widget/app_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:github_home_widget/screen/home_screen.dart';
 
-void main() {
+// Be sure to annotate your callback function to avoid issues in release mode on Flutter >= 3.3.0
+@pragma('vm:entry-point')
+void printHello() {
+  final DateTime now = DateTime.now();
+  final int isolateId = Isolate.current.hashCode;
+  print("[$now] Hello, world! isolate=${isolateId} function='$printHello'");
+}
+
+final appWidgetPlugin = AppWidgetPlugin(
+  androidPackageName: 'tech.noxasch.app_widget_example',
+);
+main() async {
+  // Be sure to add this line if initialize() call happens before runApp()
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await AndroidAlarmManager.initialize();
   runApp(const MyApp());
+  final int helloAlarmID = 0;
+  await AndroidAlarmManager.periodic(
+      const Duration(minutes: 1), helloAlarmID, printHello);
 }
 
 class MyApp extends StatelessWidget {
